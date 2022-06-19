e6d94fbcfc416db7dc5a37ef9b7110456bee7e7d
Kaushal Joshi <7joshikaushal@gmail.com>
2022-06-19 18:17:17 +0530

    feat: copy button added

--- src/Code.jsx
+++ src/Code.jsx
@@ -1,13 +1,27 @@
 import { useCodeContext } from "./codeContext.jsx";
 import SyntaxHighlighter from "react-syntax-highlighter";
 
+const handleCopy = (code) => {
+  navigator.clipboard.writeText(code);
+};
+
 export default function Code(props) {
   const { code } = useCodeContext();
   return (
     <>
-      {/* <section className="code">{code && <pre>{code}</pre>}</section> */}
       <section className="code">
-        {code && <SyntaxHighlighter language="css">{code}</SyntaxHighlighter>}
+        {code && (
+          <>
+            <SyntaxHighlighter language="css">{code}</SyntaxHighlighter>
+            <button
+              type="button"
+              onClick={() => handleCopy(code)}
+              className="copy-button"
+            >
+              Copy
+            </button>
+          </>
+        )}
       </section>
     </>
   );
